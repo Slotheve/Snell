02d0959a9dba297b83e830d9be723e5baeb3251b
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-06-02 16:28:55 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -273,9 +273,9 @@ check_sys
 menu() {
 	clear
 	echo "######################################"
-	echo -e "#         ${RED}Snell一键安装脚本${PLAIN}          #"
-	echo -e "#    ${GREEN}作者${PLAIN}: 怠惰(Slotheve)            #"
-	echo -e "#    ${GREEN}网址${PLAIN}: https://slotheve.com      #"
+	echo -e "#         ${RED}Snell一键安装脚本${PLAIN}             #"
+	echo -e "#    ${GREEN}作者${PLAIN}: 怠惰(Slotheve)             #"
+	echo -e "#    ${GREEN}网址${PLAIN}: https://slotheve.com       #"
 	echo -e "#    ${GREEN}TG群${PLAIN}: https://t.me/slotheve     #"
 	echo "######################################"
 	echo " -------------"
